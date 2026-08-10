3cf32d9839de04948bb6efd97bc71df2b7bde22c
chaya-git <chayas008@gmail.com>
2026-08-10 17:01:39 +0530

    Update script.js

--- Robo_certificate-main/RobomanthanCertificationPortal-main/frontend/script.js
+++ Robo_certificate-main/RobomanthanCertificationPortal-main/frontend/script.js
@@ -5,7 +5,7 @@ const API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-   : "https://robo-certificate.onrender.com"; // <-- change this after deploying the backend
+   : "https://updated-certificate.onrender.com"; // <-- change this after deploying the backend
 
 if (window.location.pathname.includes("admin.html")) {
   if (localStorage.getItem("adminAccess") !== "true") {
